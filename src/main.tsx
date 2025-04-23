09e746fa367a975059308e9c602514dee429996c
Mamuthvel <mamuthvel@gmail.com>
2025-04-23 11:57:06 +0530

    Testing isso

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+<>
+  <div id="isso-thread" data-title="Isso Test">
+    <noscript>Javascript needs to be activated to view comments.</noscript>
+    </div>
+    <App />
+</>
+);
